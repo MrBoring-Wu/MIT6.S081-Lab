a7ada72355ecd53ad5fd298ec8ccf031afc7398f
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2022-12-08 01:33:12 -0500

    success version

--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -96,15 +98,21 @@ walkaddr(pagetable_t pagetable, uint64 va)
 {
   pte_t *pte;
   uint64 pa;
-
+  struct proc *p=myproc();
   if(va >= MAXVA)
     return 0;
 
   pte = walk(pagetable, va, 0);
-  if(pte == 0)
-    return 0;
-  if((*pte & PTE_V) == 0)
-    return 0;
+  
+  if(pte == 0||(*pte & PTE_V) == 0){
+    if(!is_lazy_allocate(va)){
+      return 0;
+    }
+    if(!lazy_allocate(p->pagetable,va)){
+        return 0;
+    }
+    
+  }
   if((*pte & PTE_U) == 0)
     return 0;
   pa = PTE2PA(*pte);
@@ -181,9 +189,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;
+      // panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;
+      // panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,9 +325,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;
+      // panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;
+      // panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
@@ -439,4 +451,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+int is_lazy_allocate(uint64 va){
+  struct proc *p=myproc();
+  if(va<p->sz&&va>=PGROUNDUP(p->trapframe->sp))
+    return 1;
+  return 0;
+}
+
+int lazy_allocate(pagetable_t pagetable_t,uint64 va)
+{
+  char *mem;
+  mem = kalloc();
+  if (mem == 0)
+  {
+    return 0;
+  }
+  memset(mem, 0, PGSIZE);
+  if (mappages(pagetable_t, PGROUNDDOWN(va), PGSIZE, (uint64)mem, PTE_W | PTE_X | PTE_R | PTE_U) != 0)
+  {
+    kfree(mem);
+    return 0;
+  }
+  return 1;
 }
